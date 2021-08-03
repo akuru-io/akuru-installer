e1ac91e77ddd727c81933ffe408188e069a88e68
Thiruni <thirudilushi@gmail.com>
2021-08-04 01:27:11 +0530

    Include menu bar icons and styles-11

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,32 +1,43 @@
 import { Link } from 'react-router-dom';
 
+
 import {
     Wrapper,
     Menu,
     MenuItem,
+    AllFontsIcon, 
+    Text,
+    InstalledFontsIcon,
+    UpdatesIcon,
+    Logo,
+
   } from './styled';
 
 export default function Nav() {
     return(
         <Wrapper>
+            <Logo />
             <Menu>
-                
-                <Link to="/">
-                <MenuItem>
-                    All Fonts
-                    </MenuItem>
-                </Link>
-               
-                <MenuItem>
-                <Link to="/">
-                   Installed
-                </Link>
-                </MenuItem>
-                <MenuItem>
-                <Link to="/">
-                    Updates
-                 </Link>
-                </MenuItem>
+               <Link  to ="/">
+               <MenuItem>
+                    <AllFontsIcon />
+                    <Text>All Fonts</Text>
+               </MenuItem>
+               </Link>
+
+               <Link to ="/">
+               <MenuItem>
+                    <InstalledFontsIcon />
+                    <Text>Installed Fonts</Text>
+               </MenuItem>
+               </Link>
+
+               <Link to ="/">
+               <MenuItem>
+                    <UpdatesIcon />
+                    <Text>Updates</Text>
+               </MenuItem>
+               </Link>
             </Menu>
             
         </Wrapper>
--- src/components/Nav/styled.js
+++ src/components/Nav/styled.js
@@ -1,22 +1,66 @@
 import styled from 'styled-components';
+import AllFontsIconSource from '../../assets/AllFontsIcon';
+import InstalledFontsIconSource from '../../assets/InstalledFontsIcon';
+import UpdatesIconSource from '../../assets/UpdatesIcon';
+import LogoSource from '../../assets/Logo';
 
 export const Wrapper = styled.div`
   width: 25%;
   background: #878383;
-  padding:30px;
+  
 `;
 
 export const Menu = styled.ul`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  width : 100%;
+  background-color:black;
   
 `;
 
 export const MenuItem = styled.li`
   cursor: pointer;
-  padding:10px;
-  background-color:red;
-  width : 100%;
+  padding:10px 10px 10px 20px;
+  display: flex;
+  flex-direction:row;
   list-style-type:none;
+  text-decoration: none;
 `;
+
+export const Text =styled.p`
+  color :#FFFFFF;
+  align-self: left;
+  font-weight: 900;
+  font-size :15px;
+  text-decoration: none;
+`;
+
+export const AllFontsIcon = styled(AllFontsIconSource)`
+  color: #FFFFFF;;
+  width: 24px;
+  align-self: left;
+  padding : 0 5px;
+  font-weight: bold;
+`;
+
+export const InstalledFontsIcon = styled(InstalledFontsIconSource)`
+  color: White;
+  width: 24px;
+  align-self: left;
+  padding : 0 5px;
+  font-weight: bold;
+`;
+
+export const UpdatesIcon = styled(UpdatesIconSource)`
+  color: White;
+  width: 24px;
+  align-self: left;
+  padding : 0 5px;
+  font-weight: bold;
+`;
+
+export const Logo = styled(LogoSource)`
+  padding: 25px;
+  width : 24px;
+  height :24px;
+`
